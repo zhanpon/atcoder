33b9e947937c2e92c4672a42ae8d0e6c957061ab
Yiyang Zhan <pon.zhan@gmail.com>
2020-05-15 21:56:26 +0900

    Solve ABC165 A

---
+++ rust/src/bin/abc165_a.rs
@@ -0,0 +1,17 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        k: i32,
+        a: i32,
+        b: i32,
+    }
+
+    for i in a..b + 1 {
+        if i % k == 0 {
+            println!("OK");
+            return;
+        }
+    }
+    println!("NG")
+}
