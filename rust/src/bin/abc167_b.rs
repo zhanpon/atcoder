0aba66d0437163f695172912e6312d732bd8d1e5
Yiyang Zhan <pon.zhan@gmail.com>
2020-05-22 20:51:44 +0900

    Solve ABC167 B

---
+++ rust/src/bin/abc167_b.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: i64,
+        b: i64,
+        _c: i64,
+        k: i64,
+    }
+
+    let answer = if k <= a {
+        k
+    } else if k <= a + b {
+        a
+    } else {
+        2 * a + b - k
+    };
+
+    println!("{}", answer);
+}
